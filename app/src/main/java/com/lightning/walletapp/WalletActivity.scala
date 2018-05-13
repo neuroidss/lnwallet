7cf48962b4c13eb98b048b8ae6140981a89682cc
anton <admin@btcontract.com>
2018-05-13 12:39:01 +0300

    Report more info in title

--- app/src/main/java/com/lightning/walletapp/WalletActivity.scala
+++ app/src/main/java/com/lightning/walletapp/WalletActivity.scala
@@ -227,10 +227,13 @@ class WalletActivity extends NfcReaderActivity with TimerActivity { me =>
   def showDenomChooser = {
     val lnTotalMsat = app.ChannelManager.notClosingOrRefunding.map(estimateTotalCanSend).sum
     val walletTotalSum = Satoshi(app.kit.conf0Balance.value + lnTotalMsat / 1000L)
-    val inFiat = msatInFiatHuman(walletTotalSum)
+    val inFiatSpendable = msatInFiatHuman(app.kit.conf1Balance)
+    val inFiatTotal = msatInFiatHuman(walletTotalSum)
 
     val title = getLayoutInflater.inflate(R.layout.frag_wallet_state, null)
     val form = getLayoutInflater.inflate(R.layout.frag_input_choose_fee, null)
+    val spendableContent = title.findViewById(R.id.spendableContent).asInstanceOf[TextView]
+    val chanFeeContent = title.findViewById(R.id.chanFeeContent).asInstanceOf[TextView]
     val stateContent = title.findViewById(R.id.stateContent).asInstanceOf[TextView]
     val denomChoiceList = form.findViewById(R.id.choiceList).asInstanceOf[ListView]
     val allDenominations = getResources.getStringArray(R.array.denoms).map(_.html)
@@ -247,15 +250,16 @@ class WalletActivity extends NfcReaderActivity with TimerActivity { me =>
 
     denomChoiceList setAdapter new ArrayAdapter(me, singleChoice, allDenominations)
     denomChoiceList.setItemChecked(app.prefs.getInt(AbstractKit.DENOM_TYPE, 0), true)
-    stateContent setText s"${coloredIn apply walletTotalSum}<br><small>$inFiat</small>".html
+    spendableContent setText s"${coloredIn apply app.kit.conf1Balance}<br><small>$inFiatSpendable</small>".html
+    stateContent setText s"${coloredIn apply walletTotalSum}<br><small>$inFiatTotal</small>".html
+    chanFeeContent setText s"${denom withSign RatesSaver.rates.feeTwo} / KB".html
     showForm(negBuilder(dialog_ok, title, form).create)
   }
 
   // SETTINGS FORM
 
   def makeSettingsForm = {
-    val feePerKb = denom withSign RatesSaver.rates.feeTwo
-    val title = getString(read_settings).format(feePerKb).html
+    val title = getString(read_settings).html
     val form = getLayoutInflater.inflate(R.layout.frag_settings, null)
     val menu = showForm(negBuilder(dialog_ok, title, form).create)
 
