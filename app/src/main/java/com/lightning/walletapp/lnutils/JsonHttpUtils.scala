b7370f9f54b41b67ed300bc844b90b191fd9f024
anton <admin@btcontract.com>
2018-05-25 09:22:26 +0300

    Send fee update to channels on getting new feerates

--- app/src/main/java/com/lightning/walletapp/lnutils/JsonHttpUtils.scala
+++ app/src/main/java/com/lightning/walletapp/lnutils/JsonHttpUtils.scala
@@ -43,7 +43,10 @@ object RatesSaver {
   def initialize = initDelay(safe, rates.stamp, 1800000) foreach { case newFee \ newFiat =>
     val sensibleSix = for (notZero <- newFee("6") +: rates.feesSix if notZero > 0) yield notZero
     val sensibleThree = for (notZero <- newFee("3") +: rates.feesThree if notZero > 0) yield notZero
-    rates = Rates(sensibleSix take 2, sensibleThree take 2, exchange = newFiat, System.currentTimeMillis)
+
+    rates = Rates(sensibleSix take 2, sensibleThree take 2, newFiat, System.currentTimeMillis)
+    // Channels may become open sooner then we get updated fees so inform channels once we get updated fees
+    for (c <- app.ChannelManager.notClosingOrRefunding) c process CMDFeerate(LNParams.broadcaster.perKwThreeSat)
     app.prefs.edit.putString(AbstractKit.RATES_DATA, rates.toJson.toString).commit
   }
 
