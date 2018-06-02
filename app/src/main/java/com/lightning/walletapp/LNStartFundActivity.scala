54252d7284b868a4a84906c256c1429641519b98
anton <admin@btcontract.com>
2018-06-02 10:49:11 +0300

    Use "MAX" button to send all available funds into a channel

--- app/src/main/java/com/lightning/walletapp/LNStartFundActivity.scala
+++ app/src/main/java/com/lightning/walletapp/LNStartFundActivity.scala
@@ -14,6 +14,7 @@ import com.lightning.walletapp.lnutils.ImplicitConversions._
 import com.lightning.walletapp.lnutils.ImplicitJsonFormats._
 import com.lightning.walletapp.lnutils.olympus.OlympusWrap
 import com.lightning.walletapp.lnutils.olympus.CloudAct
+import android.content.DialogInterface.BUTTON_NEUTRAL
 import com.lightning.walletapp.helper.AES
 import fr.acinq.bitcoin.Crypto.PublicKey
 import org.bitcoinj.script.ScriptBuilder
@@ -122,13 +123,11 @@ class LNStartFundActivity extends TimerActivity { me =>
 
     def askForFunding(their: Init): TimerTask = UITask {
       // Current feerate may be higher than hard capacity so choose the currently largest value
-      val minCap = MilliSatoshi(math.max(LNParams.broadcaster.perKwThreeSat, 250000L) * 1000L)
-      val onChainBalance: MilliSatoshi = app.kit.conf1Balance
-      val maxRecommendedCap = MilliSatoshi(16777215000L)
-
-      val canSend = denom withSign onChainBalance
-      val maxHuman = denom withSign maxRecommendedCap
-      val minHuman = denom withSign minCap
+      val minCapacity = MilliSatoshi(math.max(LNParams.broadcaster.perKwThreeSat, 250000L) * 1000L)
+      val maxCapacity = MilliSatoshi(math.min(app.kit.conf1Balance.value, 16777215L) * 1000L)
+      val canSend = denom withSign app.kit.conf1Balance
+      val minHuman = denom withSign minCapacity
+      val maxHuman = denom withSign maxCapacity
 
       val content = getLayoutInflater.inflate(R.layout.frag_input_fiat_converter, null, false)
       val rateManager = new RateManager(getString(amount_hint_newchan).format(minHuman, maxHuman, canSend), content)
@@ -156,15 +155,16 @@ class LNStartFundActivity extends TimerActivity { me =>
       }
 
       def askAttempt(alert: AlertDialog) = rateManager.result match {
-        case Success(ms) if ms > maxRecommendedCap => app toast dialog_sum_big
-        case Success(ms) if ms < minCap => app toast dialog_sum_small
+        case Success(ms) if ms > maxCapacity => app toast dialog_sum_big
+        case Success(ms) if ms < minCapacity => app toast dialog_sum_small
         case Failure(reason) => app toast dialog_sum_empty
         case Success(ms) => rm(alert)(next(ms).start)
       }
 
-      // When balance is less than or equal max chan capacity we fill it in thus giving a hint that all can be sent into channel
-      mkCheckForm(askAttempt, none, baseBuilder(getString(ln_ops_start_fund_title).html, content), dialog_next, dialog_cancel)
-      if (onChainBalance >= minCap && onChainBalance <= maxRecommendedCap) rateManager setSum Try(onChainBalance)
+      def useMax = rateManager setSum Try(maxCapacity)
+      val bld = baseBuilder(getString(ln_ops_start_fund_title).html, content).setNeutralButton("max", null)
+      val button = mkCheckForm(askAttempt, none, bld, dialog_next, dialog_cancel) getButton BUTTON_NEUTRAL
+      button setOnClickListener onButtonTap(useMax)
     }
 
     whenBackPressed = UITask {
