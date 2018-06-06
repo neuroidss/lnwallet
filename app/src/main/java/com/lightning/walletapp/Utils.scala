0bce11468e5e42c3e762fc8631e379f5109f7e49
anton <admin@btcontract.com>
2018-06-06 14:34:27 +0300

    Minor improvements

--- app/src/main/java/com/lightning/walletapp/Utils.scala
+++ app/src/main/java/com/lightning/walletapp/Utils.scala
@@ -135,10 +135,12 @@ trait TimerActivity extends AppCompatActivity { me =>
   def showForm(alertDialog: AlertDialog) = {
     // This may be called after a host activity is destroyed and thus it may throw
     alertDialog.getWindow.getAttributes.windowAnimations = R.style.SlidingDialog
-    try alertDialog.show catch none
 
-    // Could be run on tablets so constrain max window size
-    alertDialog.getWindow.setLayout(maxDialog.toInt, WRAP_CONTENT)
+    try alertDialog.show catch none finally if (scrWidth > 2.3) {
+      // Could be run on large tablets so constrain max window size
+      alertDialog.getWindow.setLayout(maxDialog.toInt, WRAP_CONTENT)
+    }
+
     alertDialog
   }
 
