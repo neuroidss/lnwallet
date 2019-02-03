bc8daecd94b66259cf4f5ef2ef3703eed93238df
anton <admin@btcontract.com>
2019-02-03 14:38:14 +0200

    Fix NodeAnnouncement bug Some announcements broadcasted by peers may not contains any public address

--- app/src/main/java/com/lightning/walletapp/ln/wire/LightningMessage.scala
+++ app/src/main/java/com/lightning/walletapp/ln/wire/LightningMessage.scala
@@ -119,19 +119,18 @@ case class NodeAnnouncement(signature: BinaryData,
                             nodeId: PublicKey, rgbColor: RGB, alias: String,
                             addresses: NodeAddressList) extends RoutingMessage {
 
-  val workingAddress = addresses.collectFirst {
+  val workingAddress = addresses collectFirst {
     case IPv4(sockAddress, port) => new InetSocketAddress(sockAddress, port)
     case IPv6(sockAddress, port) => new InetSocketAddress(sockAddress, port)
     case Tor2(address, port) => OnionAddress.fromParts(address, port).toInetSocketAddress
     case Tor3(address, port) => OnionAddress.fromParts(address, port).toInetSocketAddress
-  }.get
+  } getOrElse new InetSocketAddress("127.0.0.1", 9735)
 
-  val pretty = addresses.collectFirst {
-    case _: IPv4 => nodeId.toString take 15 grouped 3 mkString "\u0020"
-    case _: IPv6 => nodeId.toString take 15 grouped 3 mkString "\u0020"
+  val pretty = addresses collectFirst {
+    case _: IPv4 | _: IPv6 => nodeId.toString take 15 grouped 3 mkString "\u0020"
     case _: Tor2 => s"<strong>Tor</strong> ${nodeId.toString take 12 grouped 3 mkString "\u0020"}"
     case _: Tor3 => s"<strong>Tor</strong> ${nodeId.toString take 12 grouped 3 mkString "\u0020"}"
-  }.get
+  } getOrElse "No IP address"
 
   val identifier = (alias + nodeId.toString).toLowerCase
   val asString = s"<strong>${alias take 16}</strong><br><small>$pretty</small>"
